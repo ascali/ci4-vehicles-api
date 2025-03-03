fe50f1dde7acc25188eac0685c54088a408eae04
Ascaliko <ascaliko7@gmail.com>
2025-03-03 16:10:50 +0700

    init

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('/home', 'Home::home');
+$routes->get('/', 'Home::home');
+$routes->get('/home', 'Home::index');
 $routes->get('/bus', 'Home::bus');
 $routes->get('/log_bus', 'Home::log_bus');
