86d34d29685c483ab29fd0cf34aa7961b9c6749f
Ascaliko <ascaliko7@gmail.com>
2025-03-10 11:45:31 +0700

    update

--- app/Modules/Main/Config/Routes.php
+++ app/Modules/Main/Config/Routes.php
@@ -5,7 +5,7 @@ if(!isset($routes))
     $routes = \Config\Services::routes(true);
 }
 
-$routes->group('main', ['namespace' => 'App\Modules\Main\Controllers'], function ($subroutes) {
+$routes->group('api', ['namespace' => 'App\Modules\Main\Controllers'], function ($subroutes) {
     // $subroutes->add('action/(:any)', 'MainAction::$1');
     // $subroutes->add('ajax/(:any)', 'MainAjax::$1');
     $subroutes->add('', 'Main::index');
